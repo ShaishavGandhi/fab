08e5428a1c7e2550f664d1bedfb9a04b8e230de0
Shaishav Gandhi <shaishav@uber.com>
2020-03-21 23:48:31 -0700

    Add Authorization in JSON file

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,14 @@ extern crate serde_json;
 
 use clap;
 use clap::{App, SubCommand};
-use std::fs::read_to_string;
-use std::io;
-use crate::structs::FabConfig;
+use std::fs::{read_to_string, File};
+use std::{io, fs};
+use crate::structs::{FabConfig, WhoAmIResponse};
 
 mod structs;
 mod diffs;
+
+const WHO_AM_I: &str = "api/user.whoami";
 
 fn main() {
     let matches = App::new("Fab")
@@ -32,49 +34,90 @@ fn main() {
     }
 }
 
-fn init() -> Result<FabConfig, &'static str>{
-    let contents = read_to_string("~/.fab/config");
-    if contents.is_err() {
-        println!("Welcome to Fab! Let's get you started");
+fn init() -> Result<FabConfig, &'static str> {
+    let path_buf = dirs::home_dir()
+        .expect("Couldn't find home directory");
 
-        println!("Enter the URL where your Phabricator instance is hosted. Example: https://phab.mycompany.com/");
-        let mut hosted_instance = String::new();
+    let home_dir = path_buf.to_str().expect("Couldn't convert home directory to string.");
+    let fab_dir = format!("{}/.fab", home_dir);
+    let config_file = format!("{}/.fab/config.json", home_dir);
 
-        io::stdin().read_line(&mut hosted_instance)
-            .expect("Failed to read URL");
+    let contents = read_to_string(&config_file);
+    return match contents {
+        Err(_message) => {
+            println!("Welcome to Fab! Let's get you started");
 
-        // Trim newlines
-        hosted_instance = hosted_instance.trim().to_string();
+            println!("Enter the URL where your Phabricator instance is hosted. Example: https://phab.mycompany.com/");
+            let mut hosted_instance = String::new();
 
-        // Add a trailing `/` if needed.
-        if !hosted_instance.ends_with('/') {
-            hosted_instance.push('/')
+            io::stdin().read_line(&mut hosted_instance)
+                .expect("Failed to read URL");
+
+            // Trim newlines
+            hosted_instance = hosted_instance.trim().to_string();
+
+            // Add a trailing `/` if needed.
+            if !hosted_instance.ends_with('/') {
+                hosted_instance.push('/')
+            }
+
+            // Make sure hosted instance is present.
+            if hosted_instance.is_empty() {
+                return Result::Err("Hosted Instance cannot be null");
+            }
+
+            println!("Enter the API token that Phab can use. You can create one at {}settings/user/YOUR_USERNAME/page/apitokens", hosted_instance);
+            let mut api_token = String::new();
+
+            io::stdin().read_line(&mut api_token)
+                .expect("Failed to read token");
+
+            // Trim newlines
+            api_token = api_token.trim().to_string();
+
+            // Make sure API token is present.
+            if api_token.is_empty() {
+                return Result::Err("API Token cannot be null");
+            }
+
+            // Get user's details
+            let phid = match get_phid(&hosted_instance, &api_token) {
+                Ok(phid) => phid,
+                Err(message) => panic!(message)
+            };
+
+            let config = FabConfig {
+                hosted_instance,
+                api_token,
+                phid
+            };
+
+            fs::create_dir_all(&fab_dir).expect(&format!("Couldn't create directory '{}'", &fab_dir));
+            serde_json::to_writer(&File::create(config_file).expect("Couldn't load file"), &config).expect("Couldn't write config to file");
+
+            Result::Ok(config)
+        },
+        Ok(content) => {
+            let config: FabConfig = ::serde_json::from_str(&content).expect("Couldn't deserialize to FabConfig");
+            Result::Ok(config)
         }
-
-        // Make sure hosted instance is present.
-        if hosted_instance.is_empty() {
-            return Result::Err("Hosted Instance cannot be null");
-        }
-
-        println!("Enter the API token that Phab can use. You can create one at {}settings/user/YOUR_USERNAME/page/apitokens", hosted_instance);
-        let mut api_token = String::new();
-
-        io::stdin().read_line(&mut api_token)
-            .expect("Failed to read token");
-
-        // Trim newlines
-        api_token = api_token.trim().to_string();
-
-        // Make sure API token is present.
-        if api_token.is_empty() {
-            return Result::Err("API Token cannot be null");
-        }
-
-        return Result::Ok(FabConfig {
-            hosted_instance,
-            api_token
-        })
     }
+}
 
-    return Result::Err("Couldn't initialize fab");
+fn get_phid(hosted_instance: &String, api_token: &String) -> Result<String, &'static str> {
+    let url = format!("{}{}", hosted_instance, WHO_AM_I);
+    let json_body = json!({
+            "api.token": api_token
+            });
+
+    let response = reqwest::blocking::Client::new()
+        .post(&url)
+        .form(&json_body)
+        .send()
+        .expect("Error fetching user details");
+
+    return match response.json::<WhoAmIResponse>() {
+        Ok(res) => Result::Ok(res.result.phid),
+        Err(_message) => Result::Err("Error getting user's phabricator ID")
+    }
 }
