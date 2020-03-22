0f7d94b84903c27c2685bd404a3df989404054aa
Shaishav Gandhi <shaishgandhi@gmail.com>
2020-03-21 21:27:42 -0700

    Abstract Host and API Tokens (#10)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate serde_json;
 
 use clap;
 use clap::{App, SubCommand};
+use std::fs::read_to_string;
+use std::io;
+use crate::structs::PhabConfig;
 
 mod structs;
 mod diffs;
@@ -17,7 +20,61 @@ fn main() {
             .author("Shaishav <shaishavgandhi05@gmail.com>"))
         .get_matches();
 
+    let result = init();
+    let config = match result {
+       Ok(config) => config,
+       Err(message) => panic!("{}", message)
+    };
+
+
     if let Some(matches) = matches.subcommand_matches("diff") {
-        diffs::process_diff_command(matches)
+        diffs::process_diff_command(matches, &config)
     }
+}
+
+fn init() -> Result<PhabConfig, &'static str>{
+    let contents = read_to_string("~/.phab/config");
+    if contents.is_err() {
+        println!("Welcome to Phab! Let's get your started");
+
+        println!("Enter the URL where your Phabricator instance is hosted. Example: https://phab.mycompany.com/");
+        let mut hosted_instance = String::new();
+
+        io::stdin().read_line(&mut hosted_instance)
+            .expect("Failed to read URL");
+
+        // Trim newlines
+        hosted_instance = hosted_instance.trim().to_string();
+
+        // Add a trailing `/` if needed.
+        if !hosted_instance.ends_with('/') {
+            hosted_instance.push('/')
+        }
+
+        // Make sure hosted instance is present.
+        if hosted_instance.is_empty() {
+            return Result::Err("Hosted Instance cannot be null");
+        }
+
+        println!("Enter the API token that Phab can use. You can create one at {}settings/user/YOUR_USERNAME/page/apitokens", hosted_instance);
+        let mut api_token = String::new();
+
+        io::stdin().read_line(&mut api_token)
+            .expect("Failed to read token");
+
+        // Trim newlines
+        api_token = api_token.trim().to_string();
+
+        // Make sure API token is present.
+        if api_token.is_empty() {
+            return Result::Err("API Token cannot be null");
+        }
+
+        return Result::Ok(PhabConfig {
+            hosted_instance,
+            api_token
+        })
+    }
+
+    return Result::Err("Couldn't initialize phab");
 }
