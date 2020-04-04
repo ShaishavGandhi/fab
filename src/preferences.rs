750d638eff5fe441d101d4e21167cf7f86c463df
Shaishav Gandhi <shaishav@uber.com>
2020-04-03 21:33:44 -0700

    Add support for Preferences

---
+++ src/preferences.rs
@@ -0,0 +1,32 @@
+use serde::{Deserialize, Serialize};
+
+/// Get user's preferences
+pub fn get_preferences() -> Result<Preferences, String> {
+    let prefs = confy::load::<Preferences>("fab");
+    match prefs {
+        Ok(pref) => Result::Ok(pref),
+        Err(_err) => Result::Err(String::from("Couldn't load preferences"))
+    }
+}
+
+/// Stores new preferences to disk
+pub fn set_preferences(preferences: &Preferences) {
+    confy::store("fab", preferences).expect("Failed to set preferences");
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Preferences {
+    pub summary_task_priority: Vec<String>,
+    pub default_task_priority: Vec<String>,
+    pub default_limit: i32
+}
+
+impl ::std::default::Default for Preferences {
+    fn default() -> Self {
+        Self {
+            summary_task_priority: vec![String::from("high"), String::from("needs-triage")],
+            default_task_priority: vec![String::from("high")],
+            default_limit: 20
+        }
+    }
+}
--- src/summary.rs
+++ src/summary.rs
@@ -3,12 +3,18 @@ use crate::structs::FabConfig;
 use crate::diffs::{get_needs_review_diffs, render_diffs, get_authored_diffs};
 use crate::tasks::{get_tasks, Priority, render_tasks};
 use console::style;
+use crate::preferences::Preferences;
 
-pub fn process_summary(_matches: &ArgMatches, config: &FabConfig) {
+pub fn process_summary(_matches: &ArgMatches, config: &FabConfig, preferences: &Preferences) {
     let needs_review_diffs = get_needs_review_diffs(config).unwrap();
     let authored_diffs = get_authored_diffs(config).unwrap();
-    let tasks = get_tasks("10", &[Priority::get_value_for_name("high").unwrap()], config)
-        .expect("Couldn't fetch tasks");
+
+    let priorities: Vec<i32> = preferences.summary_task_priority.iter()
+        .map(|priority| Priority::get_value_for_name(&priority).unwrap())
+        .collect();
+
+    let tasks = get_tasks(preferences.default_limit.to_string().as_str(),
+        &priorities, config).expect("Couldn't fetch tasks");
 
     println!("{}", style("Diffs that need your review").bold().underlined());
     println!();
