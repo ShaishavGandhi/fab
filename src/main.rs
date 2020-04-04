3d9a623e3a8d93f1ca3ee70f2290f3a309ce751e
Shaishav Gandhi <shaishav@uber.com>
2020-04-04 00:44:31 -0700

    FMT

--- src/main.rs
+++ src/main.rs
@@ -28,38 +28,59 @@ fn main() {
     let matches = App::new("Fab")
         .author("Shaishav <shaishavgandhi05@gmail.com>")
         .version(version)
-        .subcommand(App::new("diffs")
-            .version(version)
-            .author("Shaishav <shaishavgandhi05@gmail.com>")
-            .about("Commands related to your differential revisions")
-            .arg(Arg::with_name("needs-review")
-                .short('n')
-                .long("needs-review")
-                .help("Show diffs that need your review")))
-        .subcommand(App::new("tasks")
-            .about("Commands related to maniphest tasks")
-            .version(version)
-            .author("Shaishav <shaishavgandhi05@gmail.com>")
-            .arg(Arg::with_name("priority")
-                .short('p')
-                .long("priority")
-                .possible_values(&["unbreak-now", "needs-triage", "high", "normal", "low", "wishlist"])
-                .help("Specify the priority of the task")
-                .default_values(default_task_priority)
-                .multiple(true))
-            .arg(Arg::with_name("limit")
-                .short('l')
-                .long("limit")
-                .help("limit results by a value")
-                .default_value("20")))
-        .subcommand(App::new("summary")
-            .about("Gives a snapshot of what is relevant to you in the moment")
-            .version(version)
-            .author("Shaishav <shaishavgandhi05@gmail.com>"))
-        .subcommand(App::new("configure")
-            .about("Configure settings ")
-            .version(version)
-            .author("Shaishav <shaishavgandhi05@gmail.com>"))
+        .subcommand(
+            App::new("diffs")
+                .version(version)
+                .author("Shaishav <shaishavgandhi05@gmail.com>")
+                .about("Commands related to your differential revisions")
+                .arg(
+                    Arg::with_name("needs-review")
+                        .short('n')
+                        .long("needs-review")
+                        .help("Show diffs that need your review"),
+                ),
+        )
+        .subcommand(
+            App::new("tasks")
+                .about("Commands related to maniphest tasks")
+                .version(version)
+                .author("Shaishav <shaishavgandhi05@gmail.com>")
+                .arg(
+                    Arg::with_name("priority")
+                        .short('p')
+                        .long("priority")
+                        .possible_values(&[
+                            "unbreak-now",
+                            "needs-triage",
+                            "high",
+                            "normal",
+                            "low",
+                            "wishlist",
+                        ])
+                        .help("Specify the priority of the task")
+                        .default_values(default_task_priority)
+                        .multiple(true),
+                )
+                .arg(
+                    Arg::with_name("limit")
+                        .short('l')
+                        .long("limit")
+                        .help("limit results by a value")
+                        .default_value("20"),
+                ),
+        )
+        .subcommand(
+            App::new("summary")
+                .about("Gives a snapshot of what is relevant to you in the moment")
+                .version(version)
+                .author("Shaishav <shaishavgandhi05@gmail.com>"),
+        )
+        .subcommand(
+            App::new("configure")
+                .about("Configure settings ")
+                .version(version)
+                .author("Shaishav <shaishavgandhi05@gmail.com>"),
+        )
         .get_matches();
 
     let result = auth::init();
@@ -75,6 +96,7 @@ fn main() {
     } else if let Some(matches) = matches.subcommand_matches("summary") {
         summary::process_summary(matches, &config, &preferences);
     } else if let Some(matches) = matches.subcommand_matches("configure") {
-        preferences::process_configuration(matches).expect("Failed to process configuration command");
+        preferences::process_configuration(matches)
+            .expect("Failed to process configuration command");
     }
 }
--- src/preferences.rs
+++ src/preferences.rs
@@ -1,8 +1,8 @@
-use serde::{Deserialize, Serialize};
 use clap::ArgMatches;
-use dialoguer::{Checkboxes, Input};
-use dialoguer::theme::ColorfulTheme;
 use console::style;
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::{Checkboxes, Input};
+use serde::{Deserialize, Serialize};
 use std::io;
 
 /// Get user's preferences
@@ -39,19 +39,47 @@ impl ::std::default::Default for Preferences {
 pub fn process_configuration(_matches: &ArgMatches) -> io::Result<()> {
     let current_preferences = get_preferences().expect("Couldn't get current preferences");
 
-    let possible_priorities = vec!["unbreak-now", "needs-triage", "high", "normal", "low", "wishlist"];
+    let possible_priorities = vec![
+        "unbreak-now",
+        "needs-triage",
+        "high",
+        "normal",
+        "low",
+        "wishlist",
+    ];
 
-    println!("{}", style("Choose the task priorities to include in your summary").bold().underlined());
+    println!(
+        "{}",
+        style("Choose the task priorities to include in your summary")
+            .bold()
+            .underlined()
+    );
     println!("(Press space to select a priority)");
 
-    let summary_priorities = get_chosen_priorities(&possible_priorities, &current_preferences.summary_task_priority)?;
+    let summary_priorities = get_chosen_priorities(
+        &possible_priorities,
+        &current_preferences.summary_task_priority,
+    )?;
 
-    println!("{}", style("Choose the task priorities as your default for `fab tasks`").bold().underlined());
+    println!(
+        "{}",
+        style("Choose the task priorities as your default for `fab tasks`")
+            .bold()
+            .underlined()
+    );
     println!("(Press space to select a priority)");
 
-    let default_task_priorities = get_chosen_priorities(&possible_priorities, &current_preferences.default_task_priority)?;
+    let default_task_priorities = get_chosen_priorities(
+        &possible_priorities,
+        &current_preferences.default_task_priority,
+    )?;
 
-    println!("{}", style("Choose default limit for Fab results").bold().underlined());
+    println!(
+        "{}",
+        style("Choose default limit for Fab results")
+            .bold()
+            .underlined()
+    );
 
     let default_limit = Input::with_theme(&ColorfulTheme::default())
         .with_initial_text(&current_preferences.default_limit.to_string())
@@ -60,7 +88,7 @@ pub fn process_configuration(_matches: &ArgMatches) -> io::Result<()> {
     let new_preferences = Preferences {
         summary_task_priority: summary_priorities,
         default_task_priority: default_task_priorities,
-        default_limit
+        default_limit,
     };
 
     set_preferences(&new_preferences);
@@ -68,9 +96,13 @@ pub fn process_configuration(_matches: &ArgMatches) -> io::Result<()> {
     Ok(())
 }
 
-fn get_chosen_priorities(possible_priorities: &Vec<&str>, current_priorities: &Vec<String>) -> io::Result<Vec<String>> {
+fn get_chosen_priorities(
+    possible_priorities: &Vec<&str>,
+    current_priorities: &Vec<String>,
+) -> io::Result<Vec<String>> {
     let theme = &ColorfulTheme::default();
-    let checked_priorities: Vec<(&str, bool)> = possible_priorities.iter()
+    let checked_priorities: Vec<(&str, bool)> = possible_priorities
+        .iter()
         .map(|&priority| (priority, current_priorities.contains(&priority.to_string())))
         .collect();
 
@@ -80,7 +112,7 @@ fn get_chosen_priorities(possible_priorities: &Vec<&str>, current_priorities: &V
 
     let mut chosen_priorites: Vec<String> = Vec::with_capacity(result.len());
 
-    for i in result  {
+    for i in result {
         chosen_priorites.push(possible_priorities[i].to_string())
     }
 
